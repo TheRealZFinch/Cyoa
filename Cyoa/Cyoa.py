1bf0de076838394a24ed6770575406dc81e39dc0
Olaf Sz <alexszymanski001@gmail.com>
2023-08-25 18:22:03 +0200

    Added argv

--- Cyoa/Cyoa.py
+++ Cyoa/Cyoa.py
@@ -1,5 +1,6 @@
 from lib.keyboard import keyboard
 from lib.imgprinter import imgprinter
+from sys import argv
 import platform
 
 if platform.system() == "Windows":
@@ -10,4 +11,3 @@ if platform.system() == "Linux" or platform.system() == "Darwin":
     clear = "clear"
 
 kb = keyboard()
-
